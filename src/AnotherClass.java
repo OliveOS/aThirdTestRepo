ba6fbda0d8ff4e2686aff4af72dac8a0033d0674
Oliver Wooding <oliverwooding@icloud.com>
2020-11-16 09:05:54 +0100

    Initial commit

---
+++ src/AnotherClass.java
@@ -0,0 +1,35 @@
+public class AnotherClass {
+    private int someThing;
+    private boolean otherThing;
+    private char thing;
+
+    public AnotherClass(int someThing, boolean otherThing, char thing) {
+        this.someThing = someThing;
+        this.otherThing = otherThing;
+        this.thing = thing;
+    }
+
+    public int getSomeThing() {
+        return someThing;
+    }
+
+    public void setSomeThing(int someThing) {
+        this.someThing = someThing;
+    }
+
+    public boolean isOtherThing() {
+        return otherThing;
+    }
+
+    public void setOtherThing(boolean otherThing) {
+        this.otherThing = otherThing;
+    }
+
+    public char getThing() {
+        return thing;
+    }
+
+    public void setThing(char thing) {
+        this.thing = thing;
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,45 @@
+public class User {
+    private String email;
+    private int age;
+    private String lastName;
+    private String firstName;
+
+    public User(String email, int age, String lastName, String firstName) {
+        this.email = email;
+        this.age = age;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
