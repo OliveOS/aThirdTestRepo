47b763f0085d9233630ffe08421da4a4998ad0ff
Oliver Wooding <oliverwooding@icloud.com>
2020-11-16 12:37:48 +0100

    adding methods/constructor/data to ThirdClass

---
+++ src/BoringClass.java
@@ -0,0 +1,35 @@
+public class BoringClass {
+    private int x;
+    private String y;
+    private boolean z;
+
+    public BoringClass(int x, String y, boolean z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public String getY() {
+        return y;
+    }
+
+    public void setY(String y) {
+        this.y = y;
+    }
+
+    public boolean isZ() {
+        return z;
+    }
+
+    public void setZ(boolean z) {
+        this.z = z;
+    }
+}
