8f316c1f99ae98b23a4f9b1e648e77feafb4932c
Oliver Wooding <oliverwooding@icloud.com>
2020-11-16 09:11:33 +0100

    adding methods/constructor/data to ThirdClass

---
+++ src/ThirdClass.java
@@ -0,0 +1,31 @@
+public class ThirdClass {
+    private int yeah;
+    private String nah;
+    private final boolean ABSOLUTE;
+
+    public ThirdClass(int yeah, String nah, boolean ABSOLUTE) {
+        this.yeah = yeah;
+        this.nah = nah;
+        this.ABSOLUTE = ABSOLUTE;
+    }
+
+    public int getYeah() {
+        return yeah;
+    }
+
+    public void setYeah(int yeah) {
+        this.yeah = yeah;
+    }
+
+    public String getNah() {
+        return nah;
+    }
+
+    public void setNah(String nah) {
+        this.nah = nah;
+    }
+
+    public boolean isABSOLUTE() {
+        return ABSOLUTE;
+    }
+}
